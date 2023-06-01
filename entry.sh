4300d38acec7143e4350c3b92b077092774a8bd3
Mark Nielsen <mark.nielsen@blackboard.com>
2023-06-01 11:57:38 -0700

    feat: add setup_jfrog.sh to entry.sh

--- entry.sh
+++ entry.sh
@@ -6,3 +6,8 @@ dir=$(dirname "${BASH_SOURCE[0]}")
 "$dir/setup_pip.sh"
 "$dir/setup_npm.sh"
 "$dir/setup_yarn.sh"
+
+# Only attempt to automatically configure JFrog if we have the Artifactory URL
+if [ -n "$ARTIFACTORY_URL" ]; then
+  "$dir/setup_jfrog.sh"
+fi
