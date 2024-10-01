70e00453c4c7aa86bd23b10adb23a01190bec97e
Michal Klouda <michal.klouda@blackboard.com>
2024-10-01 10:09:31 +0200

    setup SBT

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Configures package managers to authenticate to Artifactory.
 Every configuration option gets set via environment variables to help improve security and
 to allow usage in other CI/CD systems.
 
-Example for configuring `pip`, `npm`, `yarn` and `mvn`:
+Example for configuring `pip`, `npm`, `yarn`, `sbt` and `mvn`:
 
 ```yaml
 - name: Setup Artifactory
@@ -26,15 +26,19 @@ control.
 Additional environment variables supported by this action:
 
 | Name                       | How to use                                      |
-| -------------------------- | ----------------------------------------------- |
+| -------------------------- |-------------------------------------------------|
 | `ARTIFACTORY_SETUP_PIP`    | Set to `false` to not setup `pip`               |
 | `ARTIFACTORY_SETUP_NPM`    | Set to `false` to not setup `npm` and `yarn@v1` |
 | `ARTIFACTORY_SETUP_YARN`   | Set to `false` to not setup `yarn@v2+`          |
 | `ARTIFACTORY_SETUP_MVN`    | Set to `false` to not setup `nvm`               |
+| `ARTIFACTORY_SETUP_SBT`    | Set to `false` to not setup `sbt`               |
+| `SBT_CREDENTIALS`          | The value must be "$HOME/.sbt/.credentials"     |
 | `ARTIFACTORY_SETUP_JFROG`  | Set to `true` to setup `jf` (JFrog CLI)\*       |
 | `ARTIFACTORY_PYPI_INDEX`   | Set to override PyPi index URL                  |
 | `ARTIFACTORY_NPM_REGISTRY` | Set to override NPM registry URL                |
 | `ARTIFACTORY_NPM_SCOPES`   | CSV of NPM scopes\*\*                           |
+
+
 
 \* The `ARTIFACTORY_SETUP_JFROG=true` only applies when using the `entry.sh` which is
 called when using GitHub Actions. When calling `setup_jfrog.sh` directly, you do not need
@@ -66,6 +70,7 @@ git clone --quiet --depth 1 --branch v2 https://github.com/blackboard-innersourc
 ./gh-action-setup-artifactory/setup_npm.sh
 ./gh-action-setup-artifactory/setup_yarn.sh
 ./gh-action-setup-artifactory/setup_mvn.sh
+./gh-action-setup-artifactory/setup_sbt.sh
 ./gh-action-setup-artifactory/setup_jfrog.sh
 ```
 
---
+++ setup_sbt.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+: "
+This script setups your environment for Scala SBT connected to JFrog. It creates files:
+- $HOME/.sbt/.credentials
+- $HOME/.sbt/repositories
+- $HOME/.sbt/1.0/plugins/credentials.sbt
+
+Besides the two basic variables (ARTIFACTORY_USERNAME, ARTIFACTORY_TOKEN), it requires SBT env variable SBT_CREDENTIALS.
+"
+
+require_var() {
+  if [ -z "$1" ]; then
+    >&2 echo "$2"
+    return 1
+  fi
+  return 0
+}
+
+require_env() {
+  require_var "$2" "Missing env var '$1'"
+}
+
+setup_sbt() {
+  if [ "$ARTIFACTORY_SETUP_SBT" == "false" ]; then
+    echo "Skipping SBT setup because ARTIFACTORY_SETUP_SBT=$ARTIFACTORY_SETUP_SBT"
+    return 0
+  fi
+
+  require_env "ARTIFACTORY_USERNAME" "r" || return 1
+  require_env "ARTIFACTORY_TOKEN" "$ARTIFACTORY_TOKEN" || return 1
+  require_env "SBT_CREDENTIALS" "$SBT_CREDENTIALS" || return 1
+
+  local sbt_credentials_expected
+  sbt_credentials_expected="$HOME/.sbt/.credentials"
+
+  if [[ "$SBT_CREDENTIALS" != "$sbt_credentials_expected" ]]
+    then
+      echo "Env variable SBT_CREDENTIALS expected value is $sbt_credentials_expected but got $SBT_CREDENTIALS"
+      exit 1
+  fi
+
+  local dot_credentials
+  local sbt_path
+  sbt_path="$HOME/.sbt"
+  dot_credentials="${sbt_path}/.credentials"
+
+  if [[ ! -d "${sbt_path}" ]];then
+    mkdir "${sbt_path}"
+  fi
+
+  cat > "$dot_credentials" << EOF
+realm=Artifactory Realm
+host=blackboard.jfrog.io
+user=$ARTIFACTORY_USERNAME
+password=$ARTIFACTORY_TOKEN
+EOF
+
+  echo "Wrote to $dot_credentials"
+
+  local repositories
+  repositories="${sbt_path}/repositories"
+
+  cat > "$repositories" << EOF
+[repositories]
+local
+my-ivy-proxy-releases: https://blackboard.jfrog.io/artifactory/fnds-sbt/, [organization]/[module]/(scala_[scalaVersion]/)(sbt_[sbtVersion]/)[revision]/[type]s/[artifact](-[classifier]).[ext]
+my-maven-proxy-releases: https://blackboard.jfrog.io/artifactory/fnds-sbt/
+EOF
+
+  echo "Wrote to $repositories"
+
+  local credentials_sbt
+  credentials_sbt="${sbt_path}/1.0/plugins/credentials.sbt"
+
+  cat > "$credentials_sbt" << EOF
+credentials += Credentials(Path.userHome / ".sbt" / ".credentials")
+EOF
+
+  echo "Wrote to $credentials_sbt"
+
+  return 0
+}
+
+if [ "${BASH_SOURCE[0]}" = "${0}" ]; then
+  if setup_sbt "$@"; then
+    exit 0
+  fi
+  exit 1
+fi
