ae7e0fe98721ba78b44f52c0f357d2cc95d8b945
Mark Nielsen <mark.nielsen@anthology.com>
2025-03-17 14:15:52 -0700

    fix: streamline entry script to directly call setup functions

--- entry.sh
+++ entry.sh
@@ -3,15 +3,22 @@
 set -e
 
 dir=$(dirname "${BASH_SOURCE[0]}")
-"$dir/setup_pip.sh"
-"$dir/setup_npm.sh"
-"$dir/setup_yarn.sh"
-"$dir/setup_mvn.sh"
-"$dir/setup_sbt.sh"
 
-# In general, folks do not need to setup JFrog CLI and it's slow to setup - so only do so on request
+. "$dir/setup_pip.sh"
+. "$dir/setup_npm.sh"
+. "$dir/setup_yarn.sh"
+. "$dir/setup_mvn.sh"
+. "$dir/setup_sbt.sh"
+
+setup_pip
+setup_npm
+setup_yarn
+setup_mvn
+setup_sbt
+
 if [ "$ARTIFACTORY_SETUP_JFROG" == "true" ]; then
-  "$dir/setup_jfrog.sh"
+  . "$dir/setup_jfrog.sh"
+  setup_jfrog
 else
   echo "Skipping jf setup because ARTIFACTORY_SETUP_JFROG!=true"
 fi
